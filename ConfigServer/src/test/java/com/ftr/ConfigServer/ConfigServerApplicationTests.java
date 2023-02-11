c1c273bbcf77a6612959135d82b6359c13d1f1c4
Manish Sharma <manisharmaonly4u@gmail.com>
2023-02-11 13:00:08 +0530

    config server initiated

---
+++ ConfigServer/src/test/java/com/ftr/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftr.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
